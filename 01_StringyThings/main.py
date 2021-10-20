acd9afd9449ea10d34a61d137fe173d4310689b9
Andre Rademacher <andre.rademacher@check24.de>
2021-10-20 16:57:18 +0200

    Initial commit testing local setup with Python 3.10

---
+++ 01_StringyThings/main.py
@@ -0,0 +1,11 @@
+import sys
+
+
+def main():
+    print('this is spartaaa!')
+
+
+# idiomatic usage of main entry point
+# see https://docs.python.org/3/library/__main__.html
+if __name__ == '__main__':
+    sys.exit(main())
